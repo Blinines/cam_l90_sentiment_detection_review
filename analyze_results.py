fixed sign test bug

analyze_results.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
-from helpers import get_accuracy, sign_test, p_value
+import numpy as np
+from helpers import get_accuracy, sign_test, p_value, get_variance
 
 with open('results_nb.json') as json_file:
     data = json.load(json_file)
@@ -10,10 +11,13 @@ acc = {}
 for t in data.keys():
     acc[t] = {}
     for smoothing in data[t].keys():
-        acc[t][smoothing] = {'l': [], 'val': None}
+        acc[t][smoothing] = {'l': [], 'val': None, 'randomed_l': []}
         for test_fold in data[t][smoothing].keys():
             acc[t][smoothing]['l'].append(get_accuracy(data[t][smoothing][test_fold]))
-        acc[t][smoothing]['val'] = float(sum(acc[t][smoothing]['l'])) / len(acc[t][smoothing]['l'])
+            #acc[t][smoothing]['randomed_l'].append(data[t][smoothing]['random'])
+        acc[t][smoothing]['val'] = np.mean(acc[t][smoothing]['l'])
+        acc[t][smoothing]['variance'] = get_variance(acc[t][smoothing]['l'])
+        #acc[t][smoothing]['randomed'] = np.mean(acc[t][smoothing]['randomed_l'])
 
 
 def concat_prediction(data):
@@ -25,19 +29,14 @@ def concat_prediction(data):
             concat[new_name] = {}
             for val in ['NEG', 'POS']:
                 concat[new_name][val] = []
-                for key in ["0"]:
-                # for key in curr_res.keys():
+                for key in curr_res.keys():
                     concat[new_name][val] += curr_res[key][val]
     return concat
 
 
 
 # printing results
-<<<<<<< HEAD
 print_result_acc = True
-=======
-print_result_acc = False
->>>>>>> 170fe6d9bf916a951c174e8488d51634a0250cca
 if print_result_acc:
     for t in acc.keys():
         for smoothing in data[t].keys():
@@ -45,16 +44,13 @@ if print_result_acc:
             print(acc[t][smoothing])
             print('')
 
-<<<<<<< HEAD
-print_sign_test = False
-=======
 print_sign_test = True
->>>>>>> 170fe6d9bf916a951c174e8488d51634a0250cca
 if print_sign_test:
     concat = concat_prediction(data)
     models = sorted(concat.keys())
-    for model_1 in models:
-        for model_2 in models:
-            if model_2 != model_1:
-                p_val = p_value(numbers=sign_test(concat[model_1], concat[model_2]), q=0.5)
-                print("Sign test : 1 => {0}, 2 => {1}, p value is {2}".format(model_1, model_2, p_val))
+    for i in range(len(models)-1):
+        model_1 = models[i]
+        for model_2 in models[i+1:]:
+                numbers = sign_test(concat[model_1], concat[model_2])
+                p_val = p_value(numbers=numbers, q=0.5)
+                print("Sign test : 1 => {0}, 2 => {1}, p value is {2}, numbers are {3}".format(model_1, model_2, p_val, numbers))

helpers.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+from decimal import Decimal
+
+def get_variance(l):
+    # Computing variance of list
+    mean_val = np.mean(l)
+    sum_val = [None] * len(l)
+    for index, val in enumerate(l):
+        sum_val[index] = (val - mean_val)**2
+    return np.sum(sum_val)
 
 def get_accuracy(y):
 	count, tot = 0, 0
@@ -28,9 +38,9 @@ def sign_test(y_1, y_2):
             numbers['null'] += 1
     
     for i in range(len(y_1['POS'])):
-        if y_1['POS'][i] == 1 and y_2['NEG'][i] == 0:
+        if y_1['POS'][i] == 1 and y_2['POS'][i] == 0:
             numbers['plus'] += 1
-        elif y_1['POS'][i] == 0 and y_2['NEG'][i] == 1:
+        elif y_1['POS'][i] == 0 and y_2['POS'][i] == 1:
             numbers['minus'] += 1
         else:
             numbers['null'] += 1
@@ -56,8 +66,8 @@ def p_value(numbers, q):
 
     res = 0
     for i in range(k+1):
-        res += binomial(N, i) * (q**i) * ((1-q)**(N-i))
-    return 2*res
+        res += Decimal(binomial(N, i)) * Decimal((q**i)) * Decimal(((1-q)**(N-i)))
+    return float(2*res)
 
 
 # q = 0.5

svm.py
@@ -1,46 +1,42 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 from sklearn import svm
 from gensim.models.doc2vec import Doc2Vec
-from helpers_cv import sep_train_test
+from helpers_cv import folder_round_robin
 from settings import PATH_NEG_TAG, PATH_POS_TAG
-from helpers_nb import create_feat_with_s, create_feat_no_s
+from helpers_nb import create_feat_no_s
+from pipeline_svm import Doc2VecModel
 
-X_train_neg, X_test_blind_neg = sep_train_test(files_path=PATH_NEG_TAG, top_value_train=900)
-X_train_pos, X_test_blind_pos = sep_train_test(files_path=PATH_POS_TAG, top_value_train=900)
+
+fold_rr_neg = folder_round_robin(files_path=PATH_NEG_TAG, mod=10)
+X_train_neg = []
+for i in range(1, 10):
+    X_train_neg += fold_rr_neg[i]
+
+fold_rr_pos = folder_round_robin(files_path=PATH_POS_TAG, mod=10)
+X_train_pos = []
+for i in range(1, 10):
+    X_train_pos += fold_rr_pos[i]
 
 X_train = X_train_neg + X_train_pos
 y_train = [0]*len(X_train_neg) + [1]*len(X_train_pos)
+
+X_test_blind_neg = fold_rr_neg[0]
+X_test_blind_pos = fold_rr_pos[0]
 X_test_blind = X_test_blind_neg + X_test_blind_pos
 y_test_blind = [0]*len(X_test_blind_neg) + [1]*len(X_test_blind_pos)
 
 
 # Loading model
-model = Doc2Vec.load("models_svm/first_model")
+doc2vec_model = Doc2VecModel(dm=0, vector_size=100, window=4, epochs=20, hs=1)
+doc2vec_model.fit()
 
-# test_corpus = list(read_corpus(X_train, tokens_only=True))
-# blind_corpus = list(read_corpus(X_test_blind, tokens_only=True))
-
-X_train_vectors = []
-for elt in X_train:
-    features = create_feat_no_s(feat_with_s=create_feat_with_s(file_path=elt))
-    X_train_vectors.append(model.infer_vector(features))
+X_train_vectors = doc2vec_model.transform(X_train)
+X_test_vectors = doc2vec_model.transform(X_test_blind)
 
 
-X_test_blind_vectors = []
-for elt in X_test_blind:
-    features = create_feat_no_s(feat_with_s=create_feat_with_s(file_path=elt))
-    X_test_blind_vectors.append(model.infer_vector(features))
-
-clf = svm.SVC(gamma='scale')
+clf = svm.SVC(C=0.1, degree=3, gamma='auto', kernel='linear')
 clf.fit(X_train_vectors, y_train) 
-y_predicted = clf.predict(X_test_blind_vectors)
+y_predicted = clf.predict(X_test_vectors)
 
-correct = 0
-for index, elt in enumerate(y_predicted):
-    if elt == y_test_blind[index]:
-        correct += 1
-print("Accuracy: {0}".format(float(correct)/len(y_predicted)))
-
-
-
-
+print("Accuracy: {0}".format(np.mean(y_predicted==y_test_blind)))